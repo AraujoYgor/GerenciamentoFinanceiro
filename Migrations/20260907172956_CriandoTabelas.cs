606ea4509bb7fe3d59c9107b8e2067f680b6fdc6
Ygor Araujo <ygorcavalcantib@gmail.com>
2026-09-07 22:54:38 -0300

    Primeiro commit iniciado o projeto criado as classes e o banco de dados

---
+++ Migrations/20260907172956_CriandoTabelas.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GerenciamentoFinanceiro.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriandoTabelas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    CategoriaId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.CategoriaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transacoes",
+                columns: table => new
+                {
+                    TransacaoId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transacoes", x => x.TransacaoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Finacas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Valor = table.Column<double>(type: "float", nullable: false),
+                    DataDaOperacao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CategoriaId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    TransacaoId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Transacao = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Finacas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Finacas_Categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "CategoriaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categorias",
+                columns: new[] { "CategoriaId", "Nome" },
+                values: new object[,]
+                {
+                    { "comissao", "Comissão" },
+                    { "educacao", "Educação" },
+                    { "mercado", "Mercado" },
+                    { "salario", "Salário" },
+                    { "viagem", "Viagem" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Transacoes",
+                columns: new[] { "TransacaoId", "Nome" },
+                values: new object[,]
+                {
+                    { "ganho", "Ganho" },
+                    { "gasto", "Gasto" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Finacas_CategoriaId",
+                table: "Finacas",
+                column: "CategoriaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Finacas");
+
+            migrationBuilder.DropTable(
+                name: "Transacoes");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
---
+++ Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace GerenciamentoFinanceiro.Models
+{
+    public class Categoria
+    {
+        public string CategoriaId { get; set; }
+
+        public string Nome { get; set; }
+
+    }
+}
